45ff7e78616cad6e39c801a6325ea52c6d892a55
Abderrazak Chahid <abderrazak.chahid@gmail.com>
2024-10-31 22:22:34 -0400

    Update test_modules.py

--- tests/test_modules.py
+++ tests/test_modules.py
@@ -8,8 +8,8 @@ sys.path.append("src")
 # check if we are on GITHUB_ACTIONS
 IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
 
-@pytest.mark.skip(IN_GITHUB_ACTIONS, "Test doesn't work in Github Actions.")
-@pytest.mark.skipif(sys.version_info < (3, 8), reason="Requires Python 3.8 or later.")
+@pytest.mark.skipif(IN_GITHUB_ACTIONS, "Test doesn't work in Github Actions.")
+# @pytest.mark.skipif(sys.version_info < (3, 8), reason="Requires Python 3.8 or later.")
 class Test_module1:
     def test_main(self):
         assert True
