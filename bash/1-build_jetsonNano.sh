5e069f4343fbf211bfed30e4bd4544d71c08bd65
Abderrazak Chahid <abderrazak.chahid@gmail.com>
2024-12-22 00:36:13 -0500

    update bash

--- bash/1-build_jetsonNano.sh
+++ bash/1-build_jetsonNano.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 #### -----------------------   PREPARING THE WORKSPACE  -------------------------------
 docker system prune -f
 clear
@@ -14,4 +14,4 @@ echo " #################################################" && echo
 #### -----------------------   BUILDING THE PROJECT DOCKER  -------------------------------
 # build  the the dev-envirnment container(s)
 echo && echo "[${PROJECT_NAME}][Docker-Jetson] Building the container(s) for Jetson"
-docker-compose  -p "${PROJECT_NAME}" -f docker-compose_jetson.yml up -d --build 
+docker compose  -p "${PROJECT_NAME}" -f docker-compose_jetson.yml up -d --build 
